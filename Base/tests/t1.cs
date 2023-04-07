a403b7c2198ee20bcd4c543cb9d99bf28f197530
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-04-07 08:53:39 +0000

    stuff

---
+++ Base/tests/t1.cs
@@ -0,0 +1,8 @@
+using System;
+class Program {
+  public static void Main(){
+    print = Console.WriteLine;
+    print("Hello World!");
+    // this will work because print is now a method
+  } 
+}
